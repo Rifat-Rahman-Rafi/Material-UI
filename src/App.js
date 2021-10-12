6e458ca127b6ecfb4e216378da77439b40e05922
Rifat-Rahman-Rafi <rifatrahmanrafi47@gmail.com>
2021-10-12 19:52:12 +0600

    grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
-import { Typography,Box,Button,Paper, Container } from '@material-ui/core';
+import { Typography,Box,Button,Paper, Container, Grid} from '@material-ui/core';
 import { orange } from '@material-ui/core/colors';
 function App() {
   
   return (
+    <>
       <Container maxWidth="sm">
         <Paper variant="outlined">
           <Box p={4}>
@@ -14,9 +15,74 @@ function App() {
           <Button variant="contained" color="secondary">
             Learn More
           </Button>
+ <br/> <br/> 
+          <Button variant="outlined" color="secondary" fullWidth >
+            Learn More
+          </Button>
+
+
           </Box>
         </Paper>
       </Container>
+
+  <br></br>
+  <br></br>
+     
+    
+   
+
+
+
+
+
+     
+
+    <Container>
+      <Grid container spacing={4}>
+      <Grid item lg={3} md={6} sm={6} xs={12}>
+        <Paper>
+        <Typography variant='h5'>HEDER 1</Typography>
+        <Typography variant="subtitle1">hdfguihighusdfhguisfhgiusdfhgiu</Typography>
+        <Button variant="outlined" color="secondary"  >
+            Learn More
+          </Button>
+        </Paper>
+      </Grid>
+      <Grid item lg={3} sm={6} xs={12}>
+        <Paper>
+        <Typography variant='h5'>HEDER 2</Typography>
+        <Typography variant="subtitle1">hdfguihighusdfhguisfhgiusdfhgiu</Typography>
+        <Button variant="outlined" color="secondary"  >
+            Learn More
+          </Button>
+        </Paper>
+      </Grid>
+      <Grid item lg={3} sm={6} xs={12}>
+        <Paper>
+        <Typography variant='h5'>HEDER 3</Typography>
+        <Typography variant="subtitle1">hdfguihighusdfhguisfhgiusdfhgiu</Typography>
+        <Button variant="outlined" color="secondary"  >
+            Learn More
+          </Button>
+        </Paper>
+      </Grid>
+      <Grid item lg={3} sm={6} xs={12}>
+        <Paper>
+        <Typography variant='h5'>HEDER 4</Typography>
+        <Typography variant="subtitle1">hdfguihighusdfhguisfhgiusdfhgiu</Typography>
+         <Box p={2} align="center">
+        <Button variant="outlined" color="secondary" >
+            Learn More
+          </Button>
+          </Box>
+        </Paper>
+      </Grid>
+      </Grid>
+    </Container>    
+
+
+    </>
+
   );
 }
 
