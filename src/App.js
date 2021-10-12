3b67125a6cf21c222b5ac5f9795f525b3e55f009
Rifat-Rahman-Rafi <rifatrahmanrafi47@gmail.com>
2021-10-12 22:17:24 +0600

    form page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 
 import './App.css';
-import { Typography,Box,Button,Paper, Container, Grid} from '@material-ui/core';
+import { Typography,Box,Button,Paper, Container, Grid, TextField,InputAdornment} from '@material-ui/core';
 import { orange } from '@material-ui/core/colors';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 function App() {
   
   return (
@@ -67,7 +68,7 @@ function App() {
         </Paper>
       </Grid>
       <Grid item lg={3} sm={6} xs={12}>
-        <Paper>
+        <Paper elevation={4}>
         <Typography variant='h5'>HEDER 4</Typography>
         <Typography variant="subtitle1">hdfguihighusdfhguisfhgiusdfhgiu</Typography>
          <Box p={2} align="center">
@@ -80,6 +81,54 @@ function App() {
       </Grid>
     </Container>    
 
+       <br></br>
+       <br></br>
+
+
+      <Container >
+        <Paper component={Box} width="45%" mx="auto"p={4} align="center" elevation={4}>
+          <Typography variant="h5">Create New Account</Typography>
+          <Box component="form" mt={4}>
+            <TextField 
+             fullWidth
+             placeholder="Enter You First Name"
+             margin="normal"
+             variant="outlined"
+             color="secondary"
+             label="First Name"
+             InputProps={{
+               startAdornment:(
+                 <InputAdornment position="start">
+                 <AccountCircleIcon color="primary"></AccountCircleIcon>
+                 </InputAdornment>
+               )
+             }}
+             ></TextField>
+            <TextField
+             fullWidth placeholder="Enter You Last Name"
+             margin="normal"
+             variant="outlined"
+             color="primary"
+             label="Last Name" 
+             helperText="enter last name"
+             InputProps={{
+              startAdornment:(
+                <InputAdornment position="start">
+                <AccountCircleIcon color="secondary"></AccountCircleIcon>
+                </InputAdornment>
+              )
+            }}
+
+             ></TextField>
+
+          </Box>
+          <Box>
+            <Button variant="contained" color="secondary">
+              Sign Up
+            </Button>
+          </Box>
+        </Paper>
+      </Container>
 
     </>
 
